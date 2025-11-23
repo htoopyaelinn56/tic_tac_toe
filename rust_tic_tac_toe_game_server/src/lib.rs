7e1d95cd3d29d4e28db3caa14fc02027998040c7
htoopyaelinn56 <hplinn0@gmail.com>
2025-11-23 10:36:19 +0630

    Update dependencies and integrate Tokio for asynchronous multicast service

--- rust_tic_tac_toe_game_server/src/lib.rs
+++ rust_tic_tac_toe_game_server/src/lib.rs
@@ -1,6 +1,22 @@
 use godot::prelude::*;
 use rust_udp_multicast_test::multicast_service;
 
+use std::sync::OnceLock;
+use tokio::runtime::Runtime;
+
+static TOKIO_RUNTIME: OnceLock<Runtime> = OnceLock::new();
+
+fn get_runtime() -> &'static Runtime {
+    TOKIO_RUNTIME.get_or_init(|| Runtime::new().expect("Failed to create Tokio runtime"))
+}
+
+pub fn spawn<F>(future: F)
+where
+    F: std::future::Future<Output = ()> + Send + 'static,
+{
+    get_runtime().spawn(future);
+}
+
 struct MyExtension;
 
 #[gdextension]
@@ -26,15 +42,19 @@ impl RustNode {
     fn hello_world(&self) {
         godot_print!("Hello from Rust!");
     }
-    
+
     #[func]
-    fn start_discovery_service(&self, player_name: GString)  {
-        multicast_service::start_service_sync(player_name.to_string());
+    fn start_discovery_service(&self, player_name: GString) {
+        spawn(async move {
+            multicast_service::start_service(player_name.to_string()).await;
+        })
     }
 
-    #[func] 
-    fn discover_peers() -> GString {
-        let peers =  multicast_service::get_peers_sync();
-        peers.to_godot()
+    #[func]
+    fn discover_peers()  {
+        spawn(async move {
+            let peers = multicast_service::get_peers().await;
+            godot_print!("Discovered peer: {}", peers);
+        });
     }
 }
